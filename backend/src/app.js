83b0379cc2d84406dd75b3f47fc0f87f6783961c
Casey Escovedo <caseyjescovedo@gmail.com>
2025-10-26 16:25:40 +0000

    feat: user tracking

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { eventRoutes } from './routes/events.js'
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(cors())
 
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
