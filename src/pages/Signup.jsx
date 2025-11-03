e859c94737fabbe8a9673b8f36a6c5dddce72b71
Casey Escovedo <caseyjescovedo@gmail.com>
2025-11-03 04:00:21 +0000

    feat: graphql

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,30 +1,34 @@
 import { useState } from 'react'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation as useGraphQLMutation } from '@apollo/client/react/index.js'
+import { SIGNUP_USER } from '../api/graphql/users.js'
 import { useNavigate, Link } from 'react-router-dom'
-import { signup } from '../api/users.js'
-
+//import { useMutation } from '@tanstack/react-query'
+//import { signup } from '../api/users.js'
 export function Signup() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
-
-  const signupMutation = useMutation({
-    mutationFn: () => signup({ username, password }),
-    onSuccess: () => navigate('/login'),
+  const [signupUser, { loading }] = useGraphQLMutation(SIGNUP_USER, {
+    variables: { username, password },
+    onCompleted: () => navigate('/login'),
     onError: () => alert('failed to sign up!'),
   })
-
+  /* const signupMutation = useMutation({
+ mutationFn: () => signup({ username, password }),
+ onSuccess: () => navigate('/login'),
+onError: () => alert('failed to sign up!'),
+ }) */
   const handleSubmit = (e) => {
     e.preventDefault()
-    signupMutation.mutate()
+    signupUser() //signupMutation.mutate()
   }
-
   return (
     <form onSubmit={handleSubmit}>
       <Link to='/'>Back to main page</Link>
       <hr />
       <div>
         <label htmlFor='create-username'>Username: </label>
+
         <input
           type='text'
           name='create-username'
@@ -36,6 +40,7 @@ export function Signup() {
       <br />
       <div>
         <label htmlFor='create-password'>Password: </label>
+
         <input
           type='password'
           name='create-password'
@@ -45,10 +50,11 @@ export function Signup() {
         />
       </div>
       <br />
+
       <input
         type='submit'
-        value={signupMutation.isPending ? 'Signing up...' : 'Sign Up'}
-        disabled={!username || !password || signupMutation.isPending}
+        value={loading ? 'Signing up...' : 'Sign Up'}
+        disabled={!username || !password || loading}
       />
     </form>
   )
